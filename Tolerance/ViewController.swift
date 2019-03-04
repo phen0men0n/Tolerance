0129bb130db2a333ca9f228e15c82712e6bdae37
kalinin <o.kalinin@compassplus.com>
2019-03-04 09:08:33 +0500

    Initial Commit

---
+++ Tolerance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tolerance
+//
+//  Created by OLEG KALININ on 04.03.2019.
+//  Copyright © 2019 oki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
